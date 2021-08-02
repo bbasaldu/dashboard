615994900f60d66a2f2806bfa0bd556b80d93d11
Brian <bbasaldu@ucsc.edu>
2021-08-01 23:49:49 -0700

    fixed tooltip not showing up after last push

--- src/components/main/dashboard/VisualizationTemplates/d3Scripts/buildBarChart.js
+++ src/components/main/dashboard/VisualizationTemplates/d3Scripts/buildBarChart.js
@@ -9,35 +9,38 @@ const getBBox = (svg, text) => {
 };
 //helper function to return new filtered data based on some filters
 export const evalFilter = (filter, data, options) => {
-  const num = options.findIndex(d => d === filter)
-  let newData = Array.from(data)
-  if(num === 0){
+  const num = options.findIndex((d) => d === filter);
+  let newData = Array.from(data);
+  if (num === 0) {
     //sort abc
-    newData.sort((a, b) => a.label.localeCompare(b.label))
-
-  }
-  else if(num === 1){
+    newData.sort((a, b) => a.label.localeCompare(b.label));
+  } else if (num === 1) {
     //sort asc
-    newData.sort((a,b) => a.value - b.value)
-  }
-  else if(num === 2){
+    newData.sort((a, b) => a.value - b.value);
+  } else if (num === 2) {
     //sort desc
-    newData.sort((a,b) => b.value - a.value)
+    newData.sort((a, b) => b.value - a.value);
   }
-  return newData
-}
-export const buildBarChart = (id, data, options = null, transition = true, onDataChange) => {
+  return newData;
+};
+export const buildBarChart = (
+  id,
+  data,
+  options = null,
+  transition = true,
+  onDataChange
+) => {
   const { theme, selectionOptions } = options;
   //tooltip - for some reason css top, left dont affect it so i have to set it here
   //could increase performance(is it negligable?) by creating tooltip in react component and having it persist
   const tooltip = d3
-  .select("body")
-  .append("div")
-  .attr("class", classes.tooltip)
-  .attr("id", `${id}_tooltip`)
-  .style("opacity", 0)
-  .style('top', '0px')
-  .style('left', '0px')
+    .select("body")
+    .append("div")
+    .attr("class", classes.tooltip)
+    .attr("id", `${id}_tooltip`)
+    .style("opacity", 0)
+    .style("top", "0px")
+    .style("left", "0px");
 
   const container = d3.select(`#${id}`);
 
@@ -85,7 +88,7 @@ export const buildBarChart = (id, data, options = null, transition = true, onDat
     .call(d3.axisLeft(yScale));
 
   //bottle style for bars
-  const bottles = svg.append('g')
+  const bottles = svg.append("g");
   const buildBottles = () => {
     bottles
       .selectAll("bottles")
@@ -99,7 +102,7 @@ export const buildBarChart = (id, data, options = null, transition = true, onDat
       .attr("rx", 20)
       .attr("fill", theme.fourth);
   };
-  
+
   //might need getCoords function from here in the future
   //https://javascript.info/coordinates
   const showToolTip = (mouse, d) => {
@@ -111,18 +114,18 @@ export const buildBarChart = (id, data, options = null, transition = true, onDat
   };
 
   //handle data changes
-  const rectGroup = svg.append('g').attr('id', `${id}_rects`)
+  const rectGroup = svg.append("g").attr("id", `${id}_rects`);
   const changeData = (newData) => {
-    xScale.domain(newData.map(d => d.label))
-    xAxis.transition().duration(1000).call(d3.axisBottom(xScale))
+    xScale.domain(newData.map((d) => d.label));
+    xAxis.transition().duration(1000).call(d3.axisBottom(xScale));
     rectGroup
-      .selectAll('rect')
-      .data(newData, d => d.label)
+      .selectAll("rect")
+      .data(newData, (d) => d.label)
       .join(
         (enter) => handleEnter(enter),
         (update) => handleUpdate(update),
         (exit) => {}
-      )
+      );
   };
   //handling data updates
   const handleEnter = (enter) => {
@@ -153,65 +156,59 @@ export const buildBarChart = (id, data, options = null, transition = true, onDat
 
   const handleUpdate = (update) => {
     update
-      .transition().duration(1000)
-      .attr("x", (d) => xScale(d.label))
+      .transition()
+      .duration(1000)
+      .attr("x", (d) => xScale(d.label));
   };
-  
-  
-
-
-
-
-
 
   svg.selectAll("text").attr("color", theme.second);
   buildBottles();
-  //changeData(data);
-  const rects = rectGroup.selectAll('rect')
-
   //for highlighting bar value with tooltip
-  rects
+  //must be done after call to changeData, as rects wont render until then
+  const registerRectEvents = () => {
+    const rects = rectGroup.selectAll("rect");
+    rects
     .on("mouseenter", () => {
-      tooltip.style('opacity', 1)
+      tooltip.style("opacity", 1);
     })
     .on("mousemove", (ev, d) => {
       const mouse = d3.pointer(ev, svg);
       showToolTip(mouse, d);
     })
     .on("mouseleave", () => {
-        tooltip.style("opacity", 0);
+      tooltip.style("opacity", 0);
     });
+  }
+  //wait for visible event to fire then remove listener as its only for inital animation
+  //then do animation
+  //if its being resized(transition = false),
+  if (transition) {
+    const initalLoad = () => {
+      d3.select(`#${id}`).node().removeEventListener("isVisible", initalLoad);
+      changeData(data);
+      registerRectEvents()
+      //.addEventListener('isVisible', null) //does this work?
+    };
+    d3.select(`#${id}`).node().addEventListener("isVisible", initalLoad);
+  } else {
+    changeData(data);
+    registerRectEvents()
+  }
 
-    //wait for visible event to fire then remove listener as its only for inital animation
-    //then do animation
-    //if its being resized(transition = true),
-    if(transition){
-      const initalLoad = () => {
-        d3.select(`#${id}`).node().removeEventListener('isVisible', initalLoad)
-        changeData(data)
-        //.addEventListener('isVisible', null) //does this work?
-        
-      }
-      d3.select(`#${id}`).node().addEventListener('isVisible', initalLoad)
-    }
-    else{
-      changeData(data)
-    }
-    //listen for custom dropdown menu to change selection
-    //change data
-    d3.select(`#${id}_dropdown`).on('SelectionChange', (ev) => {
-      const newData = evalFilter(ev.target.innerHTML, data, selectionOptions)
-      changeData(newData)
-      onDataChange(newData)
-    })
+  
 
+  //listen for custom dropdown menu to change selection
+  //change data
+  d3.select(`#${id}_dropdown`).on("SelectionChange", (ev) => {
+    const newData = evalFilter(ev.target.innerHTML, data, selectionOptions);
+    changeData(newData);
+    onDataChange(newData);
+  });
 };
 
 export const removeBarChart = (id) => {
   const container = d3.select(`#${id}`);
   //to make sure we dont redraw it
-  d3.select(`#${id}_tooltip`).remove()
+  d3.select(`#${id}_tooltip`).remove();
   container.selectAll("svg").remove();
 };
-
-
