ce959a9aefac6cfaafd1d1137aefc7fddc4c271a
Brian <bbasaldu@ucsc.edu>
2021-08-15 23:15:30 -0700

    added MultiLineChart color legend for data filter

--- src/components/general/LineDropDownMenu.js
+++ src/components/general/LineDropDownMenu.js
@@ -1,43 +1,65 @@
-import { useEffect, useRef, useState } from 'react'
-import classes from './LineDropDownMenu.module.css'
+import { useEffect, useRef, useState } from "react";
+import classes from "./LineDropDownMenu.module.css";
+const pathColors = ["#98DDCA", "#D5ECC2", "#FFD3B4", "#FFAAA7", "#EDCCDC"];
 const LineDropDownMenu = (props) => {
-    const {data, id, handleDataChange} = props;
-    const btnRef = useRef()
- 
-    const [showOptions, setShowOptions] = useState(false)
+  const { data, id, handleDataChange } = props;
+  const btnRef = useRef();
 
-    const handleClick = () => {
-        setShowOptions(last => !last)
-    }
-    const onChange = (ev) => {
-        const temp = Array.from(data)
-        const index = ev.target.value
-        const showing = ev.target.checked
-        temp[index].showing = showing
-        handleDataChange(temp)
-    }
-    useEffect(() => {
-        data.forEach((d,i) => {
-            const elem = document.getElementById(`${id}_${d.label}`)
-            elem.setAttribute('checked', true)
-        });
-    }, [data, id])
-    
-    return (
-        <div className={classes.container}>
-            <button className={classes.btn} type="button" onClick={handleClick} ref={btnRef}>Select Lines</button>
-            <ul className={classes.options} style={{visibility:(showOptions?"visible":"hidden")}}>
-                {data.map((d,i) => {
-                    const labelID = `${id}_${d.label}`
-                    return (
-                        <li key={`${labelID}`}>
-                            <input type="checkbox" id={`${labelID}`} name={d.label} value={i} onClick={onChange}/>
-                            <label htmlFor={`${labelID}`}>{d.label}</label>
-                        </li>
-                    )
-                })}
-            </ul>
-        </div>
-    )
-}
+  const [showOptions, setShowOptions] = useState(false);
+
+  const handleClick = () => {
+    setShowOptions((last) => !last);
+  };
+  const onChange = (ev) => {
+    const temp = Array.from(data);
+    const index = ev.target.value;
+    const showing = ev.target.checked;
+    temp[index].showing = showing;
+    handleDataChange(temp);
+  };
+  useEffect(() => {
+    data.forEach((d, i) => {
+      const elem = document.getElementById(`${id}_${d.label}`);
+      elem.setAttribute("checked", true);
+    });
+  }, [data, id]);
+
+  return (
+    <div className={classes.container}>
+      <button
+        className={classes.btn}
+        type="button"
+        onClick={handleClick}
+        ref={btnRef}
+      >
+        Select Lines
+      </button>
+      <ul
+        className={classes.options}
+        style={{ visibility: showOptions ? "visible" : "hidden" }}
+      >
+        {data.map((d, i) => {
+          const labelID = `${id}_${d.label}`;
+          return (
+            <li key={`${labelID}`}>
+              <input
+                type="checkbox"
+                id={`${labelID}`}
+                name={d.label}
+                value={i}
+                onClick={onChange}
+              />
+              <label htmlFor={`${labelID}`}>
+                <div className={classes.lineLegend}>
+                    <div className={classes.lineLabel}>{d.label}</div>
+                    <div className={classes.lineColor} style={{backgroundColor: pathColors[i]}}></div>
+                </div>
+              </label>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 export default LineDropDownMenu;
