cdfeff25612dbfd048ff2d566810079e6656fbf2
Brian <bbasaldu@ucsc.edu>
2022-02-01 21:50:42 -0800

    finished quad card layout

--- src/components/QuadStats/index.js
+++ src/components/QuadStats/index.js
@@ -1,10 +1,18 @@
-import Card from '../Card'
-import cls from './QuadStats.module.css'
+import Card from "../Card";
+import QuadStat from "./QuadStat";
+import cls from "./QuadStats.module.css";
 const QuadStats = () => {
-    return (
-        <Card className={cls.cardClass}>
-            
-        </Card>
-    )
-}
-export default QuadStats
+  return (
+    <Card className={cls.cardClass}>
+      <div className={cls.row}>
+        <QuadStat />
+        <QuadStat />
+      </div>
+      <div className={cls.row}>
+        <QuadStat />
+        <QuadStat />
+      </div>
+    </Card>
+  );
+};
+export default QuadStats;
