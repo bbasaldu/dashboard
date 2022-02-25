1bad74fe4c83e54ed3bce31228983d67cb18b40d
Brian <bbasaldu@ucsc.edu>
2022-02-24 16:33:58 -0800

    removed old components from github;

--- src/components_old/main/dashboard/VisualizationTemplates/LegendColor.js
+++
@@ -1,9 +0,0 @@
-import classes from './LegendColor.module.css'
-const LegendColor = (props) => {
-    const {color} = props
-    return (
-        <div className={classes.container} style={{backgroundColor:color}}>
-        </div>
-    )
-}
-export default LegendColor
