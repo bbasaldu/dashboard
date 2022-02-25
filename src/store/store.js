ff5ec622d26001db145b38885bdfe4d3e9437e1b
Brian <bbasaldu@ucsc.edu>
2022-02-24 17:03:32 -0800

    removed redux and store; no need for them

--- src/store/store.js
+++
@@ -1,4 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import resizeReducer from './resizeSlice'
-const store = configureStore({reducer: {resizeState: resizeReducer}})
-export default store;
